459358d4b29cf119985b7fbba6af222f59af1e2c
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-10-07 13:50:45 -0400

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -81,9 +81,11 @@ while number<26:
     if zipped[number][0] != 0:
         while number2<zipped[number][0]:
             for char in str(zipped[number][2]):
-                print(char*(number2+1))
+                print(char*(number2+1), end="")
             number2=number2+1
         number=number+1
         number2=0
+        print()
     else:
         number=number+1
+
